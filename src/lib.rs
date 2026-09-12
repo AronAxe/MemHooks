f2b2ce34ba9692a44cbc882d86665c3e7b3bfa86
Aron Bijl <aron@bijl.eu>
2026-09-12 04:01:35 +0200

    feat: export normative maintainer API

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
 #![doc = include_str!("../docs/rust-library.md")]
 
+pub mod maintainer;
 pub mod model;
 pub mod parser;
 pub mod resolver;
 pub mod validator;
 
+pub use maintainer::{add_note, handle_event, init, MaintainerError, NoteInput};
 pub use model::{
     BackendMap, Entity, HookFrontmatter, QueryCondition, RecallQuery, Resource, StructuredEntity,
     StructuredRecallQuery, StructuredResource,
 };
-pub use parser::{parse_hook, parse_hook_str, ParseError, ParsedHook};
+pub use parser::{
+    parse_hook, parse_hook_str, render_hook, require_v2_schema, ParseError, ParsedHook,
+    SourceLocation,
+};
 pub use resolver::{
     find_root, inheritance_chain, merge_backend_maps, resolve, EffectiveQuery, ResolvedHook,
     Sourced, HOOK_FILENAME,
